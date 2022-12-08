0090e9151239b27e46e36169c5099dede10214bf
sharongloria <sharongloria96@gmail.com>
2022-12-08 02:04:50 -0800

    My first C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * Printing a string using puts function
+ */
+int main(void){
+	char *s = "\"Programming is like building a multilingual puzzle";
+
+	puts(s);
+
+	return 0;
+}
+
