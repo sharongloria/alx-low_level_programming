6954a77058fc6c41cdc9a15ef65b5ed2d2402565
sharongloria <sharongloria96@gmail.com>
2022-12-12 07:16:16 -0800

    Print size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", sizeof(two));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(three));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(four));
-	printf("Size of a float: %lu byte(s) \n", sizeof(five));
+	printf("Size of a float: %lu byte(s)\n", sizeof(five));
 	return (0);
 }
