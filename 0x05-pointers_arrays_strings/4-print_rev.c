79908666166e6a3328546d6bb7769f8c82bf6d8c
sharongloria <sharongloria96@gmail.com>
2022-12-20 04:02:39 -0800

    Print STring

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - Print string in reverse
+ * @s: string to be printed
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
